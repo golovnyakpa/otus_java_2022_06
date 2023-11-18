0fab1ad72d39dc921be0837308b21877afc02d7e
p.golovnyak <p.golovnyak@qiwi.com>
2023-11-18 13:58:12 +0300

    hw 16 first attempt

--- L26-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ L26-di/homework-template/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -1,8 +1,12 @@
 package ru.otus.appcontainer;
 
+import ru.otus.appcontainer.api.AppComponent;
 import ru.otus.appcontainer.api.AppComponentsContainer;
 import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+import ru.otus.utils.AnnotationUtils;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.util.*;
 
 public class AppComponentsContainerImpl implements AppComponentsContainer {
@@ -16,7 +20,16 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void processConfig(Class<?> configClass) {
         checkConfigClass(configClass);
-        // You code here...
+        try {
+            Object configInstance = configClass.getConstructor().newInstance();
+            List<Method> sortedAnnotatedMethods = AnnotationUtils
+                    .findAnnotatedMethods(configClass, AppComponent.class).stream()
+                    .sorted(Comparator.comparingInt(x -> x.getAnnotation(AppComponent.class).order()))
+                    .toList();
+            sortedAnnotatedMethods.forEach(x -> callConfigMethod(configInstance, x));
+        } catch (Exception e) {
+            throw new RuntimeException("Exception while config processing", e);
+        }
     }
 
     private void checkConfigClass(Class<?> configClass) {
@@ -27,11 +40,35 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
-        return null;
+        return (C) appComponents.stream()
+                .filter(x -> componentClass.isAssignableFrom(x.getClass()))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(
+                                ("No component present of type %s".formatted(componentClass.getName()))
+                        )
+                );
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
-        return null;
+        return (C) appComponentsByName.get(componentName);
+    }
+
+    private void callConfigMethod(Object instance, Method m) {
+        Parameter[] params = m.getParameters();
+        Object[] methodArgs = getMethodArgumentsArr(params);
+        try {
+            var component = m.invoke(instance, methodArgs);
+            appComponents.add(component);
+            appComponentsByName.put(m.getAnnotation(AppComponent.class).name(), component);
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    String.format("Exception while calling method %s in config class", m.getName()), e
+            );
+        }
+    }
+
+    private Object[] getMethodArgumentsArr(Parameter[] params) {
+        return Arrays.stream(params).map(x -> getAppComponent(x.getType())).toArray();
     }
 }
